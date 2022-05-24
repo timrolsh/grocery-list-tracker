4d86bdec13bad4a42b7d559352c3bcd080a6cbf7
Tim Rolshud <tim.rolsh@gmail.com>
2022-05-24 17:50:12 -0400

    useList displays items from database, no buttons

--- app.js
+++ app.js
@@ -2,18 +2,31 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const logger = require("morgan");
+const db = require("./db/db_connection");
+const fs = require("fs")
+
+// express set up with ejs
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
 app.use(logger("dev"));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
+    res.render("index");
 });
 app.get("/modifyList", (req, res) => {
-    res.sendFile(__dirname + "/views/modifyList.html");
+    res.render("modifyList");
 });
 app.get("/useList", (req, res) => {
-    res.sendFile(__dirname + "/views/useList.html");
+    db.execute(fs.readFileSync(__dirname + "/db/queries/read_table.sql", {encoding: "UTF-8"}), (error, results) => {
+        if (error) {
+            res.status(500).send(error); //Internal Server Error
+        }
+        else {
+            res.render("useList", {groceryList: results});
+        }
+    });
 });
 
 app.listen(port, () => {
